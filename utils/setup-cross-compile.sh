b13e50674a5d8ab31306bc9424020ebe3162dde8
JCM <jcm@jcm.re>
2025-10-30 05:05:11 +0100

    docker: oopsie -EOF is weird

--- utils/setup-cross-compile.sh
+++ utils/setup-cross-compile.sh
@@ -14,36 +14,36 @@ fi
 
 VERSION_CODENAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d'=' -f2)
 if [ "$TARGETARCH" == "amd64" ]; then
-    cat > /etc/apt/sources.list.d/ubuntu-$TARGETARCH.sources <<-EOF
-        Types: deb deb-src
-        Architectures: $TARGETARCH
-        URIs: http://de.archive.ubuntu.com/ubuntu/
-        Suites: $VERSION_CODENAME $VERSION_CODENAME-updates $VERSION_CODENAME-backports
-        Components: main restricted universe multiverse
-        Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+    cat > /etc/apt/sources.list.d/ubuntu-$TARGETARCH.sources <<EOF
+Types: deb deb-src
+Architectures: $TARGETARCH
+URIs: http://de.archive.ubuntu.com/ubuntu/
+Suites: $VERSION_CODENAME $VERSION_CODENAME-updates $VERSION_CODENAME-backports
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 
-        Types: deb deb-src
-        Architectures: amd64
-        URIs: http://security.ubuntu.com/ubuntu/
-        Suites: $VERSION_CODENAME-security
-        Components: main restricted universe multiverse
-        Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+Types: deb deb-src
+Architectures: amd64
+URIs: http://security.ubuntu.com/ubuntu/
+Suites: $VERSION_CODENAME-security
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 EOF
 else
-    cat > /etc/apt/sources.list.d/ubuntu-$TARGETARCH.sources <<-EOF
-        Types: deb
-        Architectures: $TARGETARCH
-        URIs: http://ports.ubuntu.com/ubuntu-ports/
-        Suites: $VERSION_CODENAME $VERSION_CODENAME-updates $VERSION_CODENAME-backports
-        Components: main universe restricted multiverse
-        Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+    cat > /etc/apt/sources.list.d/ubuntu-$TARGETARCH.sources <<EOF
+Types: deb
+Architectures: $TARGETARCH
+URIs: http://ports.ubuntu.com/ubuntu-ports/
+Suites: $VERSION_CODENAME $VERSION_CODENAME-updates $VERSION_CODENAME-backports
+Components: main universe restricted multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 
-        Types: deb
-        Architectures: $TARGETARCH
-        URIs: http://ports.ubuntu.com/ubuntu-ports/
-        Suites: $VERSION_CODENAME-security
-        Components: main universe restricted multiverse
-        Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+Types: deb
+Architectures: $TARGETARCH
+URIs: http://ports.ubuntu.com/ubuntu-ports/
+Suites: $VERSION_CODENAME-security
+Components: main universe restricted multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 EOF
 fi
 apt-get update
