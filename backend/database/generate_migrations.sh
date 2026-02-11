90c8c7ddc045892f60f5d9ebbc3eac68cc371e01
JCM <jcm@jcm.re>
2026-02-11 14:46:37 +0100

    fix: explicitly set length of string_view for database migrations

--- backend/database/generate_migrations.sh
+++ backend/database/generate_migrations.sh
@@ -15,6 +15,6 @@ done
 echo "std::array migrations = {" >> $target_file
 for file in $1/*.sql; do
     var_name=$(basename $file | sed 's/\.sql//g' | sed 's/[^a-zA-Z0-9]/_/g')
-    echo "std::string_view(migration_$var_name)," >> $target_file
+    echo "std::string_view(migration_$var_name, sizeof(migration_$var_name))," >> $target_file
 done
 echo "};" >> $target_file
